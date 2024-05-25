515826930a09c96fe8ef282f844c82fa549440a4
VarshaK <varsha.kamble61@gmail.com>
2024-05-25 18:41:00 +0100

    Modified Using Pylint

--- Sets.py
+++ Sets.py
@@ -1,4 +1,5 @@
-# A set is a unique collection of objects in Python. You can denote a set with a pair of {}. 
+"""Sets"""
+# A set is a unique collection of objects in Python. You can denote a set with a pair of {}.
 # Python will automatically remove duplicate items:
 
 # Create a set
@@ -8,7 +9,7 @@ print(set1)
 # Convert list to set
 album_list = [ "Michael Jackson", "Thriller", 1982, "00:42:19", \
               "Pop, Rock, R&B", 46.0, 65, "30-Nov-82", None, 10.0]
-album_set = set(album_list)             
+album_set = set(album_list)
 print(album_set)
 
 music_genres = set(["pop", "pop", "rock", "folk rock", "hard rock", "soul", \
@@ -45,20 +46,23 @@ intersection = album_set1 & album_set2
 print("Intersection is : ",intersection)
 
 # Use intersection method to find the intersection of album_list1 and album_list2
-print("Intersecion:",album_set1.intersection(album_set2))   
+print("Intersecion:",album_set1.intersection(album_set2))
 
 # Find the difference in set1 but not set2
-print("Only Album1 elements: ",album_set1.difference(album_set2)) # all the elements that are only contained in album_set1 
-print("Only Album2 elements: ",album_set2.difference(album_set1)) #The elements in album_set2 but not in album_set1
+# All the elements that are only contained in album_set1
+print("Only Album1 elements: ",album_set1.difference(album_set2))
+# The elements in album_set2 but not in album_set1
+print("Only Album2 elements: ",album_set2.difference(album_set1))
 
 # Find the union of two sets
-print("Union : ",album_set1.union(album_set2)) #The union corresponds to all the elements in both sets
+#The union corresponds to all the elements in both sets
+print("Union : ",album_set1.union(album_set2))
+
 
 # Check if superset
 print(set(album_set1).issuperset(album_set2))
 print(album_set1.issuperset({"Back in Black", "AC/DC"}) )
 
 # Check if subset
-print(set(album_set2).issubset(album_set1))  
-print(set({"Back in Black", "AC/DC"}).issubset(album_set1) )
-
+print(set(album_set2).issubset(album_set1))
+print(set({"Back in Black", "AC/DC"}).issubset(album_set1))
